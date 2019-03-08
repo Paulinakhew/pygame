555c376f6cc676fbdbacccb73683f78d6baa91b2
Esther Khew <paulinakhew12345@hotmail.com>
2019-03-08 18:51:04 -0500

    :sparkles: Add enemy that falls from random x-coordinates

--- controller.py
+++ controller.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 import sys
+
 
 pygame.init()
 
@@ -7,13 +9,24 @@ pygame.init()
 WIDTH = 800
 HEIGHT = 600
 
-RED = (255, 0, 0) #rgb colour code of player
-player_pos = [400, 300] #list of coordinates of player's position
-player_size = 50 #define the player's height and width
+#rgb colour codes
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+BACKGROUND_COLOUR = (0, 0, 0)
+
+#player variables
+player_size = 50
+player_pos = [WIDTH/2, HEIGHT-2*player_size]
+
+#enemy variables
+enemy_size = 50
+enemy_pos = [random.randint(0, WIDTH-enemy_size), 0]
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 game_over = False
+
+clock = pygame.time.Clock()
 
 while not game_over:
     for event in pygame.event.get():
@@ -25,13 +38,21 @@ while not game_over:
             y = player_pos[1]
 
             if event.key == pygame.K_LEFT:
-                x -= 5
+                x -= player_size
             elif event.key == pygame.K_RIGHT:
-                x += 5
+                x += player_size
 
             player_pos = [x, y]
     
-    screen.fill((0, 0, 0))
+    screen.fill(BACKGROUND_COLOUR)
+    if enemy_pos[1] >= 0 and enemy_pos[1] < HEIGHT:
+        enemy_pos[1] += 10
+    else:
+        enemy_pos[0] = random.randint(0, WIDTH-enemy_size)
+        enemy_pos[1] = 0
+    pygame.draw.rect(screen, BLUE, (enemy_pos[0], enemy_pos[1], enemy_size, enemy_size))
     pygame.draw.rect(screen, RED, (player_pos[0], player_pos[1], player_size, player_size))
+
+    clock.tick(30)
 
     pygame.display.update()
